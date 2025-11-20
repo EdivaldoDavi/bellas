b51337cc3819d22319d02121e4119c5bf9b5ffb1
EdivaldoDavi <edidavi@gmail.com>
2025-11-20 18:11:08 -0300

    refatorado header.tsx layout.tsx pelo dyad

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Outlet } from "react-router-dom";
 
-import Sidebar from "../sidebar/Sidebar";
+import Sidebar from "../sidebar/Sidebar"; 
 import Header from "../Header";
 import { useIsMobile } from "../../hooks/useIsMobile";
 
