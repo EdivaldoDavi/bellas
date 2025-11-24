617f6587e5227c53486a7c44a1f3fe4d274efd3c
EdivaldoDavi <edidavi@gmail.com>
2025-11-24 10:14:19 -0300

    adicionadoo whatsappdisconected , refatorado header, layout, qrcodedisplay 3

--- src/components/WhatsAppDisconnectedToast.tsx
+++ src/components/WhatsAppDisconnectedToast.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { AlertTriangle, X } from 'lucide-react';
-import styles from '../components/WhatsAppDisconnectedToast.module.css';
+import styles from '../css/WhatsAppDisconnectedToast.module.css';
 
 interface WhatsAppDisconnectedToastProps {
   closeToast?: () => void;
