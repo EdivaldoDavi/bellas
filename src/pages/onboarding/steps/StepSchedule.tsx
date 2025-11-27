42e0af85570463ac16e61cebebbdd5574274a007
EdivaldoDavi <edidavi@gmail.com>
2025-11-27 15:35:59 -0300

    refatorado stepschedule para validar servicos 2

--- src/pages/onboarding/steps/StepSchedule.tsx
+++ src/pages/onboarding/steps/StepSchedule.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState  } from "react";
 import { useUserTenant } from "../../../context/UserTenantProvider";
 import { supabase } from "../../../lib/supabaseCleint";
 import { toast } from "react-toastify";
