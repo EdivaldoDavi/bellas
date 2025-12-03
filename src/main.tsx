43a1a92846c2c308d3d0aabe0453e546b7477c24
EdivaldoDavi <edidavi@gmail.com>
2025-12-03 10:53:10 -0300

    refatorado main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom"; // Importar BrowserRouter aqui
 
 import AuthProvider from "./context/AuthProvider";
 import { UserTenantProvider } from "./context/UserTenantProvider";
@@ -21,10 +22,12 @@ registerSW({
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <UserTenantProvider>
-        <App />
-      </UserTenantProvider>
-    </AuthProvider>
+    <BrowserRouter> {/* BrowserRouter agora envolve tudo */}
+      <AuthProvider>
+        <UserTenantProvider>
+          <App />
+        </UserTenantProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
