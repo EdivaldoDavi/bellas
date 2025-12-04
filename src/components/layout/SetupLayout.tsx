7baf0437f25e889e4dae080caeb2f9aa5bd938cf
EdivaldoDavi <edidavi@gmail.com>
2025-12-04 18:48:34 -0300

    refatorado setuplayout.module, onboardingmodule.css

--- src/components/layout/SetupLayout.tsx
+++ src/components/layout/SetupLayout.tsx
@@ -4,12 +4,7 @@ import styles from "./SetupLayout.module.css";
 export default function SetupLayout({ children }: { children: ReactNode }) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.bg} />
-
-      {/* Conteúdo centralizado, mas sem card adicional */}
-      <div className={styles.content}>
-        {children}
-      </div>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 }
