8fd0b56d34ad98711c3ae8bf6eb917d1acd299fb
EdivaldoDavi <edidavi@gmail.com>
2025-12-04 17:16:37 -0300

    refatorado layoutContext e layout.tsx 42

--- src/pages/ClientesPage.tsx
+++ src/pages/ClientesPage.tsx
@@ -1,10 +1,10 @@
 // src/pages/ClientesPage.tsx
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+
 import { supabase } from "../lib/supabaseCleint";
 import { useUserAndTenant } from "../hooks/useUserAndTenant";
 
-import { X, Plus, Pencil, MessageCircle, Phone } from "lucide-react";
+import { Plus, Pencil, MessageCircle, Phone } from "lucide-react";
 import { toast } from "react-toastify";
 
 import ModalNewCustomer from "../components/ModalNewCustomer";
@@ -20,7 +20,7 @@ type Customer = {
 };
 
 export default function ClientesPage() {
-  const navigate = useNavigate();
+  
   const { tenant } = useUserAndTenant();
   const tenantId = tenant?.id;
   // const location = useLocation(); // No longer needed for page-level close
