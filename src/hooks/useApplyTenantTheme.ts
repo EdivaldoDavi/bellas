1c450e76c780281641578f80030b10d606c82cf9
EdivaldoDavi <edidavi@gmail.com>
2025-12-05 13:06:53 -0300

    refatorado useApllyTenantTheme.ts

--- src/hooks/useApplyTenantTheme.ts
+++ src/hooks/useApplyTenantTheme.ts
@@ -1,12 +1,22 @@
 // src/hooks/useApplyTenantTheme.ts
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useUserTenant } from "../context/UserTenantProvider";
 import { applyTenantTheme } from "../utils/theme";
 
 export function useApplyTenantTheme() {
   const { tenant } = useUserTenant();
+  const location = useLocation();
 
   useEffect(() => {
-    applyTenantTheme(tenant);
-  }, [tenant]);
+    const isOnboardingRoute = location.pathname.startsWith("/onboarding");
+
+    // No onboarding, aplique SEMPRE as cores padrão
+    // No restante do app, aplique as cores do tenant.
+    if (isOnboardingRoute) {
+      applyTenantTheme(null);
+    } else {
+      applyTenantTheme(tenant);
+    }
+  }, [tenant, location.pathname]);
 }
